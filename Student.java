e126c80f625d9629fd79fae11d488f95b252997f
Pratiksha Dhotre <dhotrepratiksha26@gmail.com>
2026-01-30 21:53:11 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,42 @@
+public class Student {
+
+    private int id;
+    private String name;
+    private double marks;
+
+    public Student(int id, String name, double marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ID: %d | Name: %s | Marks: %.2f", id, name, marks);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Student)) return false;
+        Student s = (Student) obj;
+        return id == s.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+}
